4bd60463b0fba86234dfe80ce26e1a6e1793f0fd
Shiori Nakama <nakamashiori717@gmail.com>
2015-05-19 18:55:08 +0900

    Initial Commit

---
+++ EZAudioSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZAudioSample
+//
+//  Created by N on 2015/05/19.
+//  Copyright (c) 2015年 Nakama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZAudioSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZAudioSample
+//
+//  Created by N on 2015/05/19.
+//  Copyright (c) 2015年 Nakama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
